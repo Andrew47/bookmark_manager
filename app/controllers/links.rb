494a2d098f5cc6467cdd746aa738ec73ec4e8f8f
Andrew Burnie <apburnie@hotmail.co.uk>
2015-12-30 15:18:03 +0000

    Refactoring

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -5,14 +5,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.create(url: params[:url], title: params[:title])
-    array = (params[:tags]).split(" ")
-    array.map do |name|
-      link.tags << Tag.create(name: name)
-    end
-    link.save
-    redirect to('/links')
-  end
+   link = Link.create(url: params[:url], title: params[:title])
+   params[:tags].split.each do |tag|
+     link.tags << Tag.create(name: tag)
+   end
+   link.save
+   redirect to('/links')
+ end
 
   get '/links/new' do
     erb :'links/new'
